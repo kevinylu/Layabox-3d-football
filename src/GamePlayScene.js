8b328ecee4ad0b4db90b2db34e8d406ad1344412
Kevin Lu <kevin@smartzeta.com>
2018-06-25 17:26:48 +1200

    Add shoot ball tween

--- src/GamePlayScene.js
+++ src/GamePlayScene.js
@@ -1,13 +1,22 @@
 CLASS$(function () {
     this.__super.call(this);
+    Laya.stage.on(Laya.Event.MOUSE_DOWN,this,onMouseDown);
 },'GamePlayScene',ui.GamePlaySceneUI);
 
-GamePlayScene.prototype.restart = function() {
-    console.log("restart");
-    settings.paused = false;
+var gameReady = false;
 
-    var relativeTween = new TWEEN.Tween(cameraPosition)
-                        .to({x:0, y:5.5, z: -12 }, 3000)
+function onMouseDown()
+{
+    if (!gameReady || settings.tweening){
+        return;
+    }
+    console.log("onMouseDown");
+    shootBall();
+}
+
+function shootBall(){
+    var relativeTween = new TWEEN.Tween(ballPosition)
+                        .to({x:4, y:4.16, z: 15 }, 500)
                         .onUpdate(function(object) {
                             settings.tweening = true;
 	                    })
@@ -15,19 +24,43 @@ GamePlayScene.prototype.restart = function() {
                             settings.tweening = false;
                         });
     relativeTween.start();
+}
+
+function setCamaraPosition() {
+    var relativeTween = new TWEEN.Tween(cameraPosition)
+                        .to({x:0, y:5.5, z: -12 }, 2000)
+                        .onUpdate(function(object) {
+                            settings.tweening = true;
+	                    })
+                        .onComplete(function(){
+                            settings.tweening = false;
+                            gameReady = true;
+                        });
+    relativeTween.start();
+}
+
+GamePlayScene.prototype.restart = function() {
     settings.paused = false;
+    console.log("restart");
+    setCamaraPosition();
 }
 
 GamePlayScene.prototype.pause = function() {
     console.log("pause");
     showGamePause();
     settings.paused = true;
+    gameReady = false;
 }
 
 GamePlayScene.prototype.tweenUpdate = function() {
     //TODO
-    camera.transform.position = cameraPosition;
-    camera.transform.lookAt(ballPosition, new Laya.Vector3(0, 180, 0), false);
+    if (!gameReady){
+        camera.transform.position = cameraPosition;
+        camera.transform.lookAt(ballPosition, new Laya.Vector3(0, 180, 0), false);
+    }
+    else{
+        ball.transform.position = ballPosition;
+    }
 }
 
 GamePlayScene.prototype.cannonUpdate = function() {
